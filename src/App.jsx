bce32dc46c2e0646586165abb50c183c47d30802
stalingYZFR6 <stalinggosling@gmail.com>
2025-10-09 09:23:12 -0600

    pruebas componente y titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Titulo from './components/Titulo.jsx';
+import Mensaje from './components/Mensaje.jsx';
+
 const App = () => {
   return (
     <>
       <h1>Ferreteria El PANTERA</h1>
+      <Titulo/>
+      <Mensaje/>
     </>
 
   );
---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Mensaje = () => {
+    return (
+        <>
+        <h1>Programar en react es tuani por:</h1>
+        <h2>  1- asaber  </h2>
+        <h2>  2- lorem ipsum </h2>
+        <h2>  3- solo mate </h2>
+       </>
+    );
+};
+
+export default Mensaje;
